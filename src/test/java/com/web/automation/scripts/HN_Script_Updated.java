d1b5f58c97d491830cf78fedad4a8b96b340cd7e
bmsyamkumar <30311414+bmsyamkumar@users.noreply.github.com>
2017-12-21 13:43:30 +0530

    ttt

--- src/test/java/com/web/automation/scripts/HN_Script_Updated.java
+++ src/test/java/com/web/automation/scripts/HN_Script_Updated.java
@@ -132,7 +132,7 @@ public class HN_Script_Updated extends TestEngineWeb {
 				
 				//Post execution status to qTest Manager
 				HN.PostInQTest(stTIme,"Passed");
-				
+				//test
 								
 		}catch (Exception e) {
 			
